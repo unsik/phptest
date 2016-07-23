b7b13cb367529d458c3880d627a0c74833daafd9
unsik <fdew@naver.com>
2016-07-24 07:20:06 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,15 +3,23 @@ $con=mysqli_connect("127.0.0.1","root","next1004","data");
  
 mysqli_set_charset($con,"utf8");
 
+if (mysqli_connect_errno($con))  
+{  
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();  
+}  
+
 $name = $_POST['name'];  
 $id = $_POST['id']; 
 $score = $_POST['score'];
 $time = $_POST['time'];
   
   
-mysqli_query($con,"insert into Person (name,id,score,lasttime) values ('$name','$id','$score','$time')");  
+
+
+
+mysqli_query(mysqli_query($con,"insert into Person (name,id,score,install) values ('$name','$id','$score','$time')");  
 mysqli_query($con,"UPDATE Person SET lasttime='$time'");
-mysqli_query($con,"UPDATE Person SET score='$score'");
+mysqli_query($con,"UPDATE Person SET score='$score'"); 
 
   
 mysqli_close($con);  
