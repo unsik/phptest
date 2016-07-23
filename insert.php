4777cb674088079ee7b2ddcd7c989ee7a4bd20ae
unsik <fdew@naver.com>
2016-07-24 07:32:13 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,26 +1,27 @@
 <?php  
-$con=mysqli_connect("127.0.0.1","root","next1004","data");  
+$con=mysqli_connect("127.0.0.1","root","패스워드","db이름");  
  
 mysqli_set_charset($con,"utf8");
-
+  
 if (mysqli_connect_errno($con))  
 {  
    echo "Failed to connect to MySQL: " . mysqli_connect_error();  
 }  
-
 $name = $_POST['name'];  
 $id = $_POST['id']; 
 $score = $_POST['score'];
-$time = $_POST['time'];
+$time = $_POST['time'];  
   
   
-
-
-
-mysqli_query($con,"insert into Person (name,id,score,install) values ('$name','$id','$score','$time');  
-mysqli_query($con,"UPDATE Person SET lasttime='$time'");
-mysqli_query($con,"UPDATE Person SET score='$score'"); 
-
+$result = mysqli_query($con,"insert into Person (name,id,score,install) values ('$name','$id','$score','$time')");  
+  mysqli_query($con,"UPDATE Person SET lasttime='$time'");
+  mysqli_query($con,"UPDATE Person SET score='$score'"); 
+  if($result){  
+    echo 'success';  
+  }  
+  else{  
+    echo 'failure';  
+  }  
   
 mysqli_close($con);  
 ?> 
