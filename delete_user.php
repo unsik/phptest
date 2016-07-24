ea0400d66ed70db38f719bb2e70ed2b1ad403f28
unsik <fdew@naver.com>
2016-07-24 16:40:53 +0900

    Delete delete_user.php

--- delete_user.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysql_connect("localhost","root","");
-mysql_select_db("phppot_examples",$conn);
-mysql_query("DELETE FROM users WHERE userId='" . $_GET["userId"] . "'");
-header("Location:list_user.php");
-?>
