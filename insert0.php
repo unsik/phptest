2bc4c2e029579056d39cce4f6afb996be1d0cb15
unsik <fdew@naver.com>
2016-07-24 17:26:20 +0900

    Update insert0.php

--- insert0.php
+++ insert0.php
@@ -35,3 +35,33 @@ if($idcheck)
 mysqli_query($con,"SELECT id, name, score, install, login, lasttime, @Rank := @Rank + 1 AS rank FROM Person CROSS JOIN (SELECT @Rank:=0) Sub0 ORDER BY score DESC");
 mysqli_close($con);  
 ?> 
+
+<?php
+$limit = 5; // 랭킹 출력 갯수
+$ii = 1; // 순위 초기화
+$query = "SELECT id, name, score FROM Person ORDER BY point DESC LIMIT 0, $limit";
+$result = mysql_query($query);
+?>
+<table border='1'>
+<tr>
+<td>rank</td>
+<td>id</td>
+<td>name</td>
+<td>score</td>
+</tr>
+<?php
+while($data_row = mysql_fetch_array($result)) {
+?>
+<tr>
+<td><?= $ii ?></td>
+<td><?= $data_row['id'] ?></td>
+<td><?= $data_row['name'] ?></td>
+<td><?= $data_row['score'] ?></td>
+</tr>
+
+
+<?php
+$ii++;
+}
+?>
+</table>
